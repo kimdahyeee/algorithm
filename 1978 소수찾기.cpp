831997a6b54f42aa4c3c433b86fd3db811aaa170
kimdahyeee <dahye4321@naver.com>
2016-10-27 18:23:21 +0900

    1978 소수찾기

---
+++ 1978 소수찾기.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int input, a, output;
+vector<int> v;
+
+
+bool prime(int t) {
+	if (t < 2) {
+		return false;
+	}
+	else {
+		for (int j = 2; j <= t / 2; j++) {
+			if (t%j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	
+}
+int main() {
+	cin >> input;
+	for (int i = 0; i < input; i++) {
+		cin >> a;
+		v.push_back(a);
+	}
+
+	for (int i = 0; i < (int)v.size(); i++) {
+		if (prime(v[i])) {
+			output++;
+		}
+	}
+
+	cout<< output;
+
+}
