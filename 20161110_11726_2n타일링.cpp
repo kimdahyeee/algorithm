7c0bdc36ed8620372f0d54d261c035213f39483d
kimdahyeee <dahye4321@naver.com>
2016-11-12 21:41:20 +0900

    20161110 algorithm study

---
+++ 20161110_11726_2n타일링.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//vector<int> v;
+//
+//int main() {
+//	int input, n;
+//	cin >> input;
+//
+//	v.push_back(1);
+//	v.push_back(2);
+//
+//	if (input >= 3) {
+//		for (int i = 2; i < input; i++) {
+//			v.push_back((v[i - 1] + v[i - 2]) % 10007);
+//		}
+//	}
+//
+//	cout << v[input - 1];
+//}
---
+++ 20161110_9095_1,2,3더하기.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include<vector>
+//using namespace std;
+//
+//int a;
+//vector<int> v;
+//int input;
+//
+//int main() {
+//	int count;
+//	cin >> count;
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(4);
+//	for (int i = 3; i < 11; i++) {
+//		v.push_back(v[i - 3] + v[i - 2] + v[i - 1]);
+//	}
+//	while (count--) {
+//		cin >> a;
+//
+//		cout << v[a - 1] <<endl;
+//	}
+//}
