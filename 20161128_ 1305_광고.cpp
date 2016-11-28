39ab21edf23e026c858ce88bf5934874023b02d3
kimdahyeee <dahye4321@naver.com>
2016-11-28 18:37:12 +0900

    20161128_ 1305광고

---
+++ 20161128_ 1305_광고.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int len;
+string word;
+int result;
+int front, tail;
+
+int ad(int l, string w) {
+	front=0, tail=1;
+	for (int i = 0; i < l-1; i++) {
+		if (w[front] == w[tail]) {
+			result ++;
+			front++;
+		}
+		else {
+			if (w[front] != w[tail]) {
+				result = 0;
+				front = 0;
+				if (w[0] == w[tail]) {
+					result ++;
+					front ++;
+				}
+			}
+		}
+		tail++;
+	}
+
+	return l - result;
+}
+
+int main() {
+	cin >> len;
+	cin >> word;
+	
+	cout << ad(len, word);
+}
