de63bdc7ccb5a6ed4149eb9c8f8ef84c181105a6
kimdahyeee <dahye4321@naver.com>
2016-12-21 14:36:20 +0900

    20161221 10828번 스택(stl사용)

---
+++ 20161221_10828번 스택(stl).cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <stack>
+#include<string>
+using namespace std;
+
+stack<int> s;
+int main() {
+	int n, push_n;
+	string req;
+	cin >> n;
+	while (n--) {
+		cin >> req;
+		if (req == "push") {
+			cin >> push_n;
+			s.push(push_n);
+		}
+		else if (req == "pop") {
+			if (s.empty()) {
+				cout << "-1" << endl;
+			}
+			else {
+				cout << s.top() << endl;
+				s.pop();
+			}
+		}else if(req == "size"){
+			cout << s.size()<<endl;
+		}
+		else if (req == "empty") {
+			cout << s.empty() << endl;
+		}else if(req == "top"){
+			if (s.empty()) {
+				cout << "-1" << endl;
+			}
+			else {
+				cout << s.top() << endl;
+			}
+		}
+	}
+}
